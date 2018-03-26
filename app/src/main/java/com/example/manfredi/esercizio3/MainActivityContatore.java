fcf73f9d6f611154bb7a0120076bf3e9d7d27668
Manfredi_Napolitano <manfnapolitano@gmail.com>
2018-03-26 15:29:04 +0200

    Terzo commit: ho effettuato i riferimenti al layout

--- app/src/main/java/com/example/manfredi/esercizio3/MainActivityContatore.java
+++ app/src/main/java/com/example/manfredi/esercizio3/MainActivityContatore.java
@@ -2,12 +2,28 @@ package com.example.manfredi.esercizio3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivityContatore extends AppCompatActivity {
+
+
+    //view layout
+
+    private TextView Messaggio;
+    private Button Aumenta;
+    private Button Diminuisci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_contatore);
+
+        //imposto i riferimenti delle view
+        Messaggio = findViewById(R.id.textMessaggio);
+        Aumenta = findViewById(R.id.buttonAumenta);
+        Diminuisci = findViewById(R.id.buttonDiminuisci);
+
     }
 }
